92f4d722b6467847eb0cbad76f6575ae48a36620
canerozus <canerozus@hotmail.com>
2021-10-21 14:30:38 +0300

    .gitignore is now working

--- src/store/weatherSlice.js
+++ src/store/weatherSlice.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
+require('dotenv').config()
 
 export const getWeather = createAsyncThunk(
     'getWeather',
