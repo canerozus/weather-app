51032e8aafc0a84a6385cbfdf7334a33ed11df8a
canerozus <canerozus@hotmail.com>
2021-10-28 05:17:37 +0300

    searchbar variant added

--- src/components/searchBar/searchBar.js
+++ src/components/searchBar/searchBar.js
@@ -1,30 +1,30 @@
-import React, {  useState } from 'react'
-import {  useDispatch } from 'react-redux'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
 import { getWeather } from '../../store/weatherSlice';
 import './searchBar.css'
 function SearchBar() {
-    const[searchValue, setSearchValue] = useState('');
+    const [searchValue, setSearchValue] = useState('');
     const dispatch = useDispatch();
 
     const onSubmit = (e) => {
         e.preventDefault();
         !searchValue ? dispatch(getWeather()) : dispatch(getWeather(searchValue))
         setSearchValue('');
-
     }
 
     return (
         <div className="search">
-        <form className="d-flex" onSubmit={onSubmit}>
-            <input onChange= {(e) => setSearchValue(e.target.value)}
-                value={searchValue}
-                type="text"
-                placeholder="Search for City" 
-                className="searchbar form-control me-2" 
-                type="search" 
-                aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
+            <form className="d-flex" onSubmit={onSubmit}>
+                <input onChange={(e) => setSearchValue(e.target.value)}
+                    variant="filled"
+                    value={searchValue}
+                    type="text"
+                    placeholder="Search for City"
+                    className="searchbar form-control me-2"
+                    type="search"
+                    aria-label="Search" />
+                <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
         </div>
     )
 }
